535fd889762fadfa31e01700184bfe6ad7c6a094
SongChun <yuwuweichun522@gmail.com>
2025-10-20 03:59:51 +0800

    fix: escape HTML entity in App component

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ const App = () => {
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="Hero Banner" />
-          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          <h1>Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy Without the Hassle</h1>
 
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
